d4d7549e4b61e656928430cdac4503f1407be0df
peterkrglv <peterkrglv@gmail.com>
2025-02-16 14:10:44 +0300

    added miniplayer

--- app/src/main/java/com/example/recordplayer/ui/MainView.kt
+++ app/src/main/java/com/example/recordplayer/ui/MainView.kt
@@ -1,13 +1,30 @@
 package com.example.recordplayer.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
+import com.example.recordplayer.data.LocalSong
+import com.example.recordplayer.domain.SongModel
 import com.example.recordplayer.ui.api_songs.ApiSongsView
 import com.example.recordplayer.ui.local_songs.LocalSongsView
 import com.example.recordplayer.ui.player.PlayerView
@@ -15,20 +32,30 @@ import com.example.recordplayer.ui.player.PlayerView
 @Composable
 fun MainView() {
     val navController = rememberNavController()
-
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     Scaffold(bottomBar = {
         BottomNavigationBar(navController)
     }) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
-            NavHost(navController = navController, startDestination = NavigationItem.Player.route) {
-                composable(NavigationItem.ApiSongs.route) {
-                    ApiSongsView()
-                }
-                composable(NavigationItem.Player.route) {
-                    PlayerView()
-                }
-                composable(NavigationItem.LocalSongs.route) {
-                    LocalSongsView()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                NavHost(
+                    navController = navController,
+                    startDestination = NavigationItem.Player.route
+                ) {
+                    composable(NavigationItem.ApiSongs.route) {
+                        ApiSongsView()
+                    }
+                    composable(NavigationItem.Player.route) {
+                        PlayerView()
+                    }
+                    composable(NavigationItem.LocalSongs.route) {
+                        LocalSongsView()
+                    }
                 }
             }
         }
